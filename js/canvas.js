811837f00188db82bb0cd8c3b1fdb3f86d44fb2e
e <573737092@qq.com>
2016-03-24 14:30:53 +0800

    resume

---
+++ js/canvas.js
@@ -0,0 +1,103 @@
+var ImageBoard = function(src,w,h,x,y){
+	this.src = src;
+	this.w = w;
+	this.h = h;
+	this.x = x;
+	this.y = y;
+	this.img = {};
+	this.oldx = x;
+	this.oldy = y;
+}
+ImageBoard.prototype.draw = function(ctx,x,y,canDraw)
+{
+	var xy;
+	if(!canDraw || !(xy = canDraw(x,y)))
+	{
+		ctx.drawImage(this.img, x || this.oldx, y || this.oldy, this.w,this.h);
+	}
+	else
+		ctx.drawImage(this.img, xy.x, xy.y, this.w,this.h);
+};
+ImageBoard.prototype.init = function(ctx)
+{
+	this.load(function(){
+		this.draw(ctx);
+	}.bind(this));
+};
+ImageBoard.prototype.load = function(callback){
+	this.img = new Image();
+	this.img.onload = callback;
+	this.img.src = this.src;
+};
+
+
+
+
+var Canvas = {
+	canvas : {},
+	ctx : {},
+	height : "",
+	width : "",
+	ctx : {},
+	img : {},
+	canDrawFuns : {},
+	init : function(){
+		this.canvas = document.getElementById('mycanvas');
+		this.width = this.canvas.width;
+		this.height = this.canvas.height;
+		this.ctx = this.canvas.getContext && this.canvas.getContext('2d');
+		if(!this.ctx)
+			return alert("动画不支持");//加载图片
+		for(var i in this.img)
+		{
+			this.img[i].init(this.ctx);
+		}
+	},
+	add : function(boardName,board,canDrawFun){
+		this.img[boardName] = board;
+		this.canDrawFuns[boardName] = canDrawFun;
+	},
+	canClear : function(boardName){
+		var clear = false;
+		for(var i in boardName)
+		{
+			if(boardName[i].x)
+				if(boardName[i].x != this.img[boardName[i].boardName].oldx)
+				{
+					clear = true;
+					this.img[boardName[i].boardName].oldx = boardName[i].x;
+				}
+			if(boardName[i].y)
+				if(boardName[i].y != this.img[boardName[i].boardName].oldy)
+				{
+					clear = true;
+					this.img[boardName[i].boardName].oldy = boardName[i].y;
+				}
+		}
+		return clear;
+	},
+	draw : function(boardName){
+		if(this.canClear(boardName))
+		{
+			this.ctx.clearRect(0,0,this.width,this.height);
+			for(var i in this.img)
+			{
+				this.img[i].draw(this.ctx,this.img[i].oldx,this.img[i].oldy,this.canDrawFuns[i]);
+			}
+		}	
+	}
+}
+
+Canvas.add("hou", new ImageBoard("img/hou1.png",200,274,0,50), function(x,y){
+	return null
+});
+Canvas.add("jingu", new ImageBoard("img/jingu.png",113,16,43.5,20), function(x,y){
+	return null
+});
+Canvas.add("eye1", new ImageBoard("img/eyes.png",7,11,63,128), function(x,y){
+	return null
+});
+Canvas.add("eye2", new ImageBoard("img/eyes.png",7,11,134,128), function(x,y){
+	return null
+});
+
--- js/index.js
+++ js/index.js
@@ -1,18 +1,22 @@
 var MonkeyAnimate = {
-	hou : null,
-	jingu : null,
+
+	hou : {},
+	jingu : {},
 	endTop : 0,
 	olds : 0,
-	nav : null,
-	navbut : null,
+	nav : {},
+	navbut : {},
 	page2Top : 0,
 	page3Top : 0,
-	divM : null,
-	secp1 : null,
-	topl : null,
-	workBut : null,
-	bottom1 : null,
-	bottom2 : null,
+	divM : {},
+	secp1 : {},
+	topl : {},
+	workBut : {},
+	bottom1 : {},
+	bottom2 : {},
+	eyes1 : {},
+	eyes2 : {},
+	canvas : {},
 	getScrollTop : function(ele){
 		var scrollPos;  
 		if(ele)
@@ -44,9 +48,10 @@ var MonkeyAnimate = {
 		}.bind(MonkeyAnimate);
 	},
 	init : function(){
-		this.hou = document.getElementById('hou');
-		this.jingu = document.getElementById('jingu');
-		this.endTop = this.getScrollTop(document.getElementById('monkey'));
+		// this.hou = document.getElementById('hou');
+		// this.jingu = document.getElementById('jingu');
+		// this.endTop = this.getScrollTop(document.getElementById('monkey'));
+		this.endTop = this.getScrollTop(document.getElementById('mycanvas'));
 		this.nav = document.getElementById('nav');
 		this.navbut = this.nav.getElementsByTagName('li');
 		this.page2Top = this.getScrollTop(document.getElementById('page2'));
@@ -57,10 +62,21 @@ var MonkeyAnimate = {
 		this.workBut = document.getElementById('top').getElementsByTagName('li');
 		this.bottom1 = document.getElementById('bottom1');
 		this.bottom2 = document.getElementById('bottom2');
+		this.eyes1 = document.getElementById('eyes1');
+		this.eyes2 = document.getElementById('eyes2');
+		this.canvas = document.getElementById('mycanvas');
 		this.bindNavButClick();
-		this.scroll();
+		
 		this.workButClick();
 		window.onscroll = this.scroll.bind(MonkeyAnimate);
+		Canvas.init();
+		this.scroll();
+		document.getElementById('page1').onmousemove = function(e)
+		{
+			e = e || window.event;
+			this.fixedEyes(e.clientX,e.clientY);
+		}.bind(this);
+
 	},
 	workButClick : function(){
 		this.workBut[0].onclick = function(){
@@ -112,7 +128,9 @@ var MonkeyAnimate = {
 		}
 	},
 	scroll : function(e){
+
 		var s = this.getScrollTop();
+
 		if(!(s < this.page2Top - 100 || s > this.page3Top + 100))
 			this.changeNavColor(true);
 		else
@@ -141,17 +159,45 @@ var MonkeyAnimate = {
 			for(var i = 3; i < 5; i++)
 				this.secp1[i].style.marginLeft = '100rem';
 		}
-		if(s - this.olds >= 10 || this.olds - s >= 5)
+		if(s - this.olds >= 10 || this.olds - s >= 10)
 		{
+			this.fixedEyes();
 			if(s <= this.endTop)
 			{
-				this.hou.style.top = -130 + s * (130 / this.endTop) + 'px';
-				this.jingu.style.top = -180 + s * (180 / this.endTop) + 'px';
+				// this.hou.style.top = -130 + s * (130 / this.endTop) + 'px';
+				// this.jingu.style.top = -180 + s * (180 / this.endTop) + 'px';
+				// this.eyes1.style.top = 128 + s * (130 / this.endTop) + 'px';
+				// this.eyes2.style.top = 128 + s * (130 / this.endTop) + 'px';
+				Canvas.draw([
+				{
+					boardName : "hou",
+					y : 50 + s * (130 / this.endTop)
+				},
+				{
+					boardName : "jingu",
+					y : 20 + s * (180 / this.endTop)
+				}
+				]);
+			
+				
 			}
 			else
 			{
-				this.hou.style.top = '0px';
-				this.jingu.style.top = '0px';
+				// this.hou.style.top = '0px';
+				// this.jingu.style.top = '0px';
+				// this.eyes1.style.top = '258px';
+				// this.eyes2.style.top = '258px';
+				Canvas.draw([
+				{
+					boardName : "hou",
+					y : 180
+				},
+				{
+					boardName : "jingu",
+					y : 200
+				}
+				]);
+		
 			}
 			if(s > this.olds)
 				this.nav.style.marginTop = "-3rem";
@@ -162,11 +208,92 @@ var MonkeyAnimate = {
 			this.olds = s;
 		}
 
-	} 
+	},
+	eyesC : function(s,rl){
+		var eye = {};
+		if(rl)
+			eye.cx = 63;
+		else
+			eye.cx = 134;
+		eye.cy = s <= this.endTop ? 128+s*(130/this.endTop) : 258;
+		eye.x = eye.cx + this.canvas.offsetLeft;
+		eye.y = this.canvas.offsetTop-s + eye.cy;
+		return eye;
+	},
+	inC : function(mx,my,cx,cy)
+	{
+		if((mx - cx)*(mx - cx) + (my - cy) * (my - cy) <= 15*15)
+			return true;
+		else
+			return false;
+	},
+	setEyes : function(eye,mx,my){
+		var x,y;
+		if(this.inC(mx,my,eye.x,eye.y))
+		{
+			x = mx - eye.x + eye.cx;
+			y = my - eye.y + eye.cy;
+		}
+		else
+		{
+			var math = (Math.sqrt((mx - eye.x)*(mx - eye.x)+(my - eye.y) * (my - eye.y)))
+			x = eye.x + 15 * ((mx - eye.x) / math);
+			y = eye.y + 15 * ((my - eye.y) / math);
+			x = x - eye.x + eye.cx;
+			y = y - eye.y + eye.cy;
+		}
+		return {x : x, y : y};
+	},
+	fixedEyes : function(mx,my){
+		var s = this.getScrollTop();
+		if(!mx || !my)
+		{
+			Canvas.draw([
+			{
+				boardName : "eye1",
+				y : s <= this.endTop ? 128 + s * (130 / this.endTop) : 258
+			},
+			{
+				boardName : "eye2",
+				y : s <= this.endTop ? 128 + s * (130 / this.endTop) : 258
+			}
+			]);
+			return;
+		}
+		var eyeL = this.eyesC(s,true);
+		var eyeR = this.eyesC(s,false);
+		var setL = this.setEyes(eyeL,mx,my);
+		var setR = this.setEyes(eyeR,mx,my);
+		Canvas.draw([
+		{
+			boardName : "eye1",
+			x : setL.x,
+			y : setL.y
+		},
+		{
+			boardName : "eye2",
+			x : setR.x,
+			y : setR.y
+		}
+		]);
+			
+	}
+
+
 
 }
 
 window.onload = MonkeyAnimate.init.bind(MonkeyAnimate);
 
 
+
+
+
+// document.getElementById('page1').onmousemove = function(e)
+// {
+// 	e = e || window.event;
+// 	console.log('x : ' + e.clientX + ' ; y : ' + e.clientY);
+// }
+
+// console.log('canvas x : ' + (this.canvas.offsetLeft+63) + ' ; y : ' + (this.canvas.offsetTop-s + 128+s * (130 / this.endTop)));
 
